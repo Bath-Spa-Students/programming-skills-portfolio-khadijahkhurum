0293121de1ea7ccd51e0239debb1f15cfc0848b7
khadijahkhurum <khadijah.khurum1@outlook.com>
2023-10-04 13:49:07 +0400

    Class Exercise

---
+++ Chapter 1- Getting Started with Python/Exercises/Class Exercise.py
@@ -0,0 +1,39 @@
+# Print W X YZ in separate lines
+print("""
+      w
+      x
+      y
+      z""")
+
+# create a program to output "hi" 50 times without any separator
+
+print ("hi"*50)
+
+#Program for a leaderboard. The program needs to output the numbers 1 to 9 each on a separate line followed by dot
+print ("""
+       1.
+       2. 
+       3. 
+       4.
+       5.
+       6.
+       7.
+       8.
+       9. """)
+
+#Compute the area of a triangle 
+b= float(input("Please enter the measurement of the base of the triangle ="))
+h = float(input ("Please enter the height of the triangle="))
+area= 1/2*(b*h)
+print ("Area of triangle is=", area)
+
+# write four strings in different variables and print the output as one string 
+a= "I"
+b= " am"
+c= " Khadijah"
+d= " Khurum"
+
+e = a + b + c+ d
+
+print (e)
+
