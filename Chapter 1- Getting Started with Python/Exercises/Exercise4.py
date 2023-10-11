9ce2ead7eadcb1e405e2ed7a0806fa6d98fa5f39
khadijahkhurum <khadijah.khurum1@outlook.com>
2023-10-11 09:56:34 +0400

    Portfoliochapter1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+
+string1 = "Hello, "
+string2 = "how are "
+string3 = "you?"
+result_string = string1 + string2 + string3
+print(result_string)
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,5 @@
+
+radius = float(input("Enter the radius of the circle: "))
+pi = 3.14159265359  
+area = pi * (radius ** 2)
+print(f"The area of the circle with radius {radius} is {area}")
