ef1cc61a664caa02a5655424f17b655b00529737
khadijahkhurum <khadijah.khurum1@outlook.com>
2023-10-11 09:39:30 +0400

    class practice.py

---
+++ Class Pratice/Untitled-1.py
@@ -0,0 +1,12 @@
+input1= "Hello"
+input2= " World"
+input3= " Code Lab 1"
+input4= input1+input2+input3
+print(input4,end= "\n")
+print("Python Coding")
+
+
+import datetime
+current_datetime = datetime.datetime.now()
+formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+print("Current Date and Time:", formatted_datetime)
