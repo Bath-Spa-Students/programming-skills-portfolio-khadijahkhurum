08f0d1392be91fd52ba44a2a328f13cd5c406ffa
khadijahkhurum <khadijah.khurum1@outlook.com>
2023-10-22 21:19:50 +0400

    chapter 2

--- Chapter 2- Variables & Comments/Exercises/Exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -1,2 +1,6 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+#Your output should look something like the following, including the quotation marks:
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.
+
 print('Emily Bronte once said,')
 print (' "I cannot express it: but surely you and everybody have a notion that there is, or should be, an existence of yours beyond you."')
--- Chapter 2- Variables & Comments/Exercises/Exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -1,3 +1,5 @@
+#Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print that message.
+
 favorite_number = 15
 message = "My favorite number is: " + str(favorite_number)
 print(message)
