2e02d52662e1c5aa863c3a9b3ea4f97be51c8c39
khadijahkhurum <khadijah.khurum1@outlook.com>
2023-11-19 08:36:32 +0400

    Update Exercise 1

--- Chapter 4- Control Flow/Exercises/Exercise 1.py
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -6,7 +6,7 @@ else:
     print("Invalid alien color.")
 
 
-    alien_color = 'red' # set a value for the variable alien_color
+alien_color = 'red' # set a value for the variable alien_color
 
 if alien_color == 'green':
     print("You earned 5 points!")
